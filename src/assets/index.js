f5606024cf33ab6acf14f3da4d7f1a77bcbf5b6b
RoshanJose <roshanstark007@gmail.com>
2023-08-19 17:20:25 +0530

    Fix: Initial Commit

---
+++ src/assets/index.js
@@ -0,0 +1,90 @@
+import logo from "./jr-logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import website from "./website.png";
+
+// Tech Skills
+
+import docker from "./tech/docker.jpeg";
+import javascript from "./tech/javascript.jpeg";
+import mongodb from "./tech/mongodb.jpeg";
+import postgresql from "./tech/postgresql.jpeg";
+import nodejs from "./tech/nodejs.jpeg";
+import reactjs from "./tech/reactjs.jpeg";
+import typescript from "./tech/typescript.jpeg";
+import gatsby from "./tech/gatsby.jpeg";
+import nextjs from "./tech/nextjs.jpeg";
+import flutter from "./tech/flutter.jpeg";
+import nestjs from "./tech/nestjs.jpeg";
+import python from "./tech/python.jpeg";
+import django from "./tech/django.jpeg";
+import aws from "./tech/aws.jpeg";
+import azure from "./tech/azure.jpeg";
+
+// Company
+import ostello from "./company/ostello.png";
+import madempty from "./company/me.png";
+import gdsc from "./company/gdsc.png";
+
+// Projects
+import remindme from "./projects/remindme.png";
+import streamit from "./projects/streamit.png";
+import focusontrack from "./projects/focusontrack.png";
+import qrgenius from "./projects/qrgenius.png";
+import mediabreeze from "./projects/mediabreeze.jpeg";
+import bbkbangalore from "./projects/bbkbangalore.png";
+import qleanbot from "./projects/qleanbot.png";
+import pypasslock from "./projects/pypasslock.png";
+import memorysnap from "./projects/memorysnap.png";
+import overflowedge from "./projects/overflowedge.png";
+
+// Socials
+import linkedin from "./socials/linkedin.png";
+import leetcode from "./socials/leetcode.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  website,
+  github,
+  menu,
+  close,
+    gatsby,
+    nextjs,
+    flutter,
+    nestjs,
+    aws,
+    azure,
+  postgresql,
+  docker,
+  javascript,
+  mongodb,
+  nodejs,
+    python,
+    django,
+  reactjs,
+  typescript,
+    madempty,
+    ostello,
+  gdsc,
+    remindme,
+  streamit,
+  focusontrack,
+    qrgenius,
+  mediabreeze,
+  bbkbangalore,
+  qleanbot,
+  pypasslock,
+  memorysnap,
+  overflowedge,
+  linkedin,
+    leetcode,
+};
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
